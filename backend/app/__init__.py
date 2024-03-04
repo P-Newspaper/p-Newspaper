02ee5d7227a7cd525dad221ec482b5114e8a483d
Aimee Co <aimeekco@gmail.com>
2024-03-03 16:54:15 -0800

    connect flask to react build directory

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path != "" and path.exists(app.static_folder + '/' + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
+    
     return app
