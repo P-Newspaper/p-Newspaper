9dea122093dfee973c62e9f11720519fb92c2c35
tyoo2025 <146300811+tyoo2025@users.noreply.github.com>
2024-03-24 19:44:21 -0700

    Update access_news.py

--- backend/app/access_news.py
+++ backend/app/access_news.py
@@ -1,4 +1,5 @@
 import newspaper
+import datetime
 
 def get_news_sites():
     """gets most popular news sites"""
@@ -6,7 +7,7 @@ def get_news_sites():
 
 def get_news_from_site(url):
     """scrapes news articles from the site and returns list containing individual dictionaries of title, keyword, summary per article"""
-    paper = newspaper.build(url, language = 'en')
+    paper = newspaper.build(url, language = 'en', memoize_articles=False)
     articleL = []
     count = 0
 
@@ -17,23 +18,75 @@ def get_news_from_site(url):
             article.parse()
             article.nlp()
             articleD['title'] = article.title
-            articleD['keywords']= article.keywords
             articleD['summary']= article.summary
+            articleD['url']=article.url
+            articleDate = article.publish_date
+            cleanDate = datetime.datetime.strptime(str(articleDate), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+            articleD['date']=cleanDate
             articleL.append(articleD)
+            count += 1
         except:
             pass
-        count += 1
-        if count >= 3:
-            break
     return articleL
 
-def get_news_from_all(siteL):
-    """gets news from a list of sources"""
+
+def get_news_from_all():
+    """gets news from a list of popular sources"""
+    siteL=get_news_sites()
     fullList = []
     for url in siteL:
         fullList += get_news_from_site(url)
-    return fullList
+    fullSorted = sort_by_date(fullList)
+    return fullSorted
 
-def get_popular_topics():
-    """popular topics at present"""
-    return newspaper.hot()
+def sort_by_date(articleL):
+    """sort list of articles (as dictionaries) by date"""
+    sortedArticles = sorted(articleL, key=lambda i: i['date'], reverse=True)
+    return sortedArticles
+
+def get_articles_lastD(articleL):
+    """get articles only from the last day"""
+    today = datetime.date.today().strftime('%Y-%m-%d')
+    dateA = sort_by_date(articleL)
+    todayArt = []
+    i = 0
+    passedDate = False
+    while i < len(dateA) and not passedDate:
+        if articleL[i]['date'] >= today:
+            todayArt.append(dateA[i])
+            i += 1
+        else:
+            passedDate = True
+    return todayArt
+
+def get_articles_lastW(articleL):
+    """get articles only from the last week"""
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    last_week = last_week.strftime('%Y-%m-%d')
+    dateA = sort_by_date(articleL)
+    weekArt = []
+    i = 0
+    passedDate = False
+    while i < len(dateA) and not passedDate:
+        if articleL[i]['date'] >= last_week:
+            weekArt.append(dateA[i])
+            i += 1
+        else:
+            passedDate = True
+    return weekArt
+
+def get_articles_lastM(articleL):
+    """get articles only from the last month"""
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    last_month = last_month.strftime('%Y-%m-%d')
+    dateA = sort_by_date(articleL)
+    monthArt = []
+    i = 0
+    passedDate = False
+    while i < len(dateA) and not passedDate:
+        if articleL[i]['date'] >= last_month:
+            monthArt.append(dateA[i])
+            i += 1
+        else:
+            passedDate = True
+    return monthArt
