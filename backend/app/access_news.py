69d6cda18c8d447fa951a05e6d5e6590d8b868fa
angelinatsai04 <antsai@g.hmc.edu>
2024-03-27 08:51:25 -0700

    changed dependencies

--- backend/app/access_news.py
+++ backend/app/access_news.py
@@ -1,4 +1,5 @@
-import newspaper
+import sys
+import newspaper3k
 import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
@@ -87,11 +88,11 @@ def get_articles_lastM(articleL):
 
 def get_news_sites():
     """gets most popular news sites"""
-    return newspaper.popular_urls()
+    return newspaper3k.popular_urls()
 
 def get_news_from_site(url):
     """scrapes news articles from the site and returns list containing individual dictionaries of title, keyword, summary per article"""
-    paper = newspaper.build(url, language = 'en', memoize_articles=False)
+    paper = newspaper3k.build(url, language = 'en', memoize_articles=False)
     articleL = []
     last_month = datetime.date.today() - datetime.timedelta(days=30)
     last_month = last_month.strftime('%Y-%m-%d')
