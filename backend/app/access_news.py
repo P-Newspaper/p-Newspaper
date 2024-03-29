92b73b5f2f845e80fd0ee3e89d9b14111cbfd05f
tyoo2025 <146300811+tyoo2025@users.noreply.github.com>
2024-03-29 13:43:07 -0700

    limited scope of scraping

--- backend/app/access_news.py
+++ backend/app/access_news.py
@@ -7,11 +7,15 @@ def opt_news_from_all():
     """Returns a list of news articles from a set of news sources;
             Each article is a dictionary with 'title' 'summary' 'url' 'date' (YYYY-MM-DD)
             Slightly faster from threading!"""
-    siteL=get_news_sites()
+    siteL=['http://www.huffingtonpost.com', 'http://cnn.com','http://www.cnbc.com','http://theatlantic.com', 
+           'http://www.bbc.co.uk','http://www.businessinsider.com','http://nytimes.com','http://yahoo.com',
+           'http://www.nbcnews.com','http://www.popsci.com','http://www.pbs.org','http://www.politico.com','http://www.reuters.com','http://www.latimes.com','http://www.forbes.com',
+           'http://washingtonpost.com']
     finalNews = []
     with ThreadPoolExecutor(max_workers=100) as executor:
         result_list = {executor.submit(get_news_from_site, (i)) for i in siteL}
         for result in as_completed(result_list):
+            print(result.result())
             finalNews+=result.result()
     return finalNews
 
@@ -87,9 +91,12 @@ def get_news_sites():
 
 def get_news_from_site(url):
     """scrapes news articles from the site and returns list containing individual dictionaries of title, keyword, summary per article"""
-    paper = newspaper.build(url, language = 'en', memoize_articles=True)
+    paper = newspaper.build(url, language = 'en', memoize_articles=False)
     articleL = []
-    count = 0
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    last_month = last_month.strftime('%Y-%m-%d')
+    i = 0
+    passedDate = False
 
     for article in paper.articles:
         try: 
@@ -97,16 +104,17 @@ def get_news_from_site(url):
             article.download()
             article.parse()
             article.nlp()
-            articleD['title'] = article.title
-            articleD['summary']= article.summary
-            articleD['url']=article.url
             articleDate = article.publish_date
             cleanDate = datetime.datetime.strptime(str(articleDate), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
-            articleD['date']=cleanDate
-            articleL.append(articleD)
-            count += 1
+            if cleanDate >= last_month:
+                articleD['date']=cleanDate
+                articleD['title'] = article.title
+                articleD['summary']= article.summary
+                articleD['url']=article.url
+                articleL.append(articleD)
+            else:
+                break
         except:
             pass
     return articleL
-
 
