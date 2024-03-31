75c36c3f8e357fcc1faae5c6a6b5f0e46298738b
Aimee Co <aimeekco@gmail.com>
2024-03-31 14:36:38 -0700

    update github link

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,17 +9,17 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer-container">
-                <a href = "https://www.p-ai.org/" className="pai">
-                    <img src={paiImage} alt="P-ai"/>
-                </a>
-                <a href = "https://www.instagram.com/pai.claremont/" className = "insta">
-                    <img src={instagram} alt="Instagram"/>
-                </a>
-                <a href = "https://github.com/p-ai-org" className = "github">
+                <a href = "https://github.com/P-Newspaper/p-Newspaper" target="_blank" rel="noopener noreferrer" className = "github">
                     <img src={github} alt="Github"/>
                 </a>
-                <a href = "https://www.linkedin.com/company/p-ai/mycompany/" className = "linkedin">
+                <a href = "https://www.p-ai.org/" target="_blank" rel="noopener noreferrer" className="pai">
+                    <img src={paiImage} alt="P-ai"/>
+                </a>
+                <a href = "https://www.linkedin.com/company/p-ai/mycompany/" target="_blank" rel="noopener noreferrer" className = "linkedin">
                     <img src={linkedin} alt="Linkedin"/>
+                </a>
+                <a href = "https://www.instagram.com/pai.claremont/" target="_blank" rel="noopener noreferrer" className = "insta">
+                    <img src={instagram} alt="Instagram"/>
                 </a>
             </div>
         </footer>
