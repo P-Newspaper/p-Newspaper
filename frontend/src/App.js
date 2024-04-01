3ddcf3e68622d618165c59ca7c844a2f7ef80f4d
angiezh <angiezhou2@gmail.com>
2024-03-31 17:03:09 -0700

    revert changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import AppRoutes from './AppRoutes';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AppRoutes from "./AppRoutes";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <AppRoutes />
-                <Footer />
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <AppRoutes />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-
