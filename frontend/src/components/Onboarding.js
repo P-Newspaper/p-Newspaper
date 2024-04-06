65f4d91bb5c42e2fbc70a6eae45ee21f62fb8f63
emizhu26 <ezki2022@mymail.pomona.edu>
2024-04-06 15:23:26 -0700

    onboarding

--- frontend/src/components/Onboarding.js
+++ frontend/src/components/Onboarding.js
@@ -1,10 +1,68 @@
 import React from 'react';
+import '../styles/onboarding.css'
+import '../styles/global.css'
 
 function Onboarding() {
     return (
-        <div>
-            <h1>Onboarding</h1>
+        <div class="everything">
+            <h2>What topics are you interested in?</h2>
+
+            <div class="grid-container">
+                <div>
+                    <img class="photo" alt="topic.png" src="images/placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div> 
+                <div>
+                    <img class="photo" alt="topic.png" src="images/placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>   
+                <div>
+                    <img class="photo" alt="topic.png" src="images/placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>   
+                <div>
+                    <img class="photo" alt="topic.png" src="images/placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>   
+                <div>
+                    <img class="photo" alt="topic.png" src="placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>   
+                <div>
+                    <img class="photo" alt="topic.png" src="placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>  
+                <div>
+                    <img class="photo" alt="topic.png" src="placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>    
+                <div>
+                    <img class="photo" alt="topic.png" src="placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>    
+                <div>
+                    <img class="photo" alt="topic.png" src="placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>    
+                <div>
+                    <img class="photo" alt="topic.png" src="placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>      
+                <div>
+                    <img class="photo" alt="topic.png" src="placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div>    
+                <div>
+                    <img class="photo" alt="topic.png" src="placeholder.png"></img>
+                    <div class="centered">Centered</div>
+                </div> 
+            </div>
+
+            {/* <button type="button" className="continue-button" onClick={handleOnboarding}>
+                Continue
+            </button> */}
         </div>
+
     );
 }
 
