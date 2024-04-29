e1df0d2d60a87e846965eed4364b528580e15403
angiezh <angiezhou2@gmail.com>
2024-04-28 20:03:14 -0700

    add user and interests to database

--- frontend/src/components/UserProvider.js
+++ frontend/src/components/UserProvider.js
@@ -4,19 +4,19 @@ import { googleLogout } from "@react-oauth/google";
 export const UserContext = createContext();
 
 function UserProvider({ children }) {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    const logout = useCallback(() => {
-        googleLogout();
-        setUser(null);
-        localStorage.removeItem("hasLoggedIn");
-    }, []);
-    
-    return (
-        <UserContext.Provider value={{ user, setUser, logout }}>
-        {children}
-        </UserContext.Provider>
-    );
+  const logout = useCallback(() => {
+    googleLogout();
+    setUser(null);
+    localStorage.removeItem("hasLoggedIn");
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
 
 export default UserProvider;
