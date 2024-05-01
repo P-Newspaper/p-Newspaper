e6ee90667aeed7977c26bd2196085df9f10acb6a
Aimee Co <aimeekco@gmail.com>
2024-04-30 20:29:28 -0700

    models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,10 @@
 from app import db
+from sqlalchemy import Column, Integer, String, Text, func
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils.types import TSVectorType
+
+Base = declarative_base()
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -15,8 +21,12 @@ class Article(db.Model):
     __tablename__ = 'news_articles'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.Date, nullable=False)
     summary = db.Column(db.Text)
     url = db.Column(db.String(255), nullable=False)
-    search_vector = db.Column(db.TsVector())
+    title_tsvector = db.Column(TSVectorType('title'))
+    
+    def __repr__(self):
+        return f'<Article {self.id}'
 
 
